a11e04fd5ffa820c8f0f4b2b251499c9bdd3d2a1
bstocker <boris.stocker@gmail.com>
2023-12-16 11:43:31 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,5 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-    @RequestMapping("/")
-    String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
-    }
 	
 }
